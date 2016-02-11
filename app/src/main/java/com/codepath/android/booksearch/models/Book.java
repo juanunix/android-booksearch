449385ab7eb13b9e26ecb6f236a3e743cac0875f
Shrikant Pandhare <spandhare@splunk.com>
2016-02-10 21:33:52 -0800

    exercise

--- app/src/main/java/com/codepath/android/booksearch/models/Book.java
+++ app/src/main/java/com/codepath/android/booksearch/models/Book.java
@@ -1,17 +1,28 @@
 package com.codepath.android.booksearch.models;
 
-import android.text.TextUtils;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.text.TextUtils;
+
 import java.util.ArrayList;
 
-public class Book {
+public class Book implements Parcelable {
     private String openLibraryId;
     private String author;
     private String title;
+    private String publisher;
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
 
     public String getOpenLibraryId() {
         return openLibraryId;
@@ -44,6 +55,7 @@ public class Book {
             }
             book.title = jsonObject.has("title_suggest") ? jsonObject.getString("title_suggest") : "";
             book.author = getAuthor(jsonObject);
+            book.publisher = getPublisher(jsonObject);
         } catch (JSONException e) {
             e.printStackTrace();
             return null;
@@ -62,6 +74,18 @@ public class Book {
                 authorStrings[i] = authors.getString(i);
             }
             return TextUtils.join(", ", authorStrings);
+        } catch (JSONException e) {
+            return "";
+        }
+    }    private static String getPublisher(final JSONObject jsonObject) {
+        try {
+            final JSONArray pubs = jsonObject.getJSONArray("publisher");
+            int numPubs = pubs.length();
+            final String[] pubStrings = new String[numPubs];
+            for (int i = 0; i < numPubs; ++i) {
+                pubStrings[i] = pubs.getString(i);
+            }
+            return TextUtils.join(", ", pubStrings);
         } catch (JSONException e) {
             return "";
         }
@@ -87,4 +111,37 @@ public class Book {
         }
         return books;
     }
+
+    public Book() {
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.openLibraryId);
+        dest.writeString(this.author);
+        dest.writeString(this.title);
+        dest.writeString(this.publisher);
+    }
+
+    private Book(Parcel in) {
+        this.openLibraryId = in.readString();
+        this.author = in.readString();
+        this.title = in.readString();
+        this.publisher = in.readString();
+    }
+
+    public static final Creator<Book> CREATOR = new Creator<Book>() {
+        public Book createFromParcel(Parcel source) {
+            return new Book(source);
+        }
+
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
 }
